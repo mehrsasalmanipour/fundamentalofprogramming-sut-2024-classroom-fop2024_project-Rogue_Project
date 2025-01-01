b6fbf25203aa8e340cca4c43a1d36e0e93fab366
mehrsasalmanipour <mehrsa.salmanipour@gmail.com>
2025-01-02 00:25:39 +0330

    Update functions.h
    Added new functions for sign up

--- functions.h
+++ functions.h
@@ -3,6 +3,11 @@
 
 void drawMenu(const char *menuItems[], int menuSize, int highlight);
 int mainMenu();
-void startGame();
+bool isValidPassword(const char *password);
+bool isValidEmail(const char *email);
+bool isUsernameTaken(const char *username);
+void displayError(int row, int col, const char *message);
+void getInputWithDisplay(int y, int x, char *input, int maxLen);
+void signUp();
 
 #endif
