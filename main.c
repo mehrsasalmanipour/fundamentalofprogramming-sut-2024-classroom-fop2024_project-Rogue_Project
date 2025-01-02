bd72b2c20e0da96d664976bfec4822f442b18879
mehrsasalmanipour <mehrsa.salmanipour@gmail.com>
2025-01-02 17:54:57 +0330

    Update main.c
    added color

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ int main() {
     if (has_colors()) {
         start_color();
         init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(2, COLOR_RED, COLOR_BLACK);
+        init_pair(3, COLOR_GREEN, COLOR_BLACK);
     }
 
     int playerIsRegistered = 0;
