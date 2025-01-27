233b4917d4bd25aaa15467c85e56cb57427d4dbc
mehrsasalmanipour <mehrsa.salmanipour@gmail.com>
2025-01-27 14:38:41 +0330

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,7 +3,7 @@
 
 #define MAX 100
 #define MAP_WIDTH 50
-#define MAP_HEIGHT 20
+#define MAP_HEIGHT 30
 #define ROOM_MIN_SIZE 4
 #define ROOM_MAX_SIZE 8
 #define MIN_ROOMS 6
@@ -19,24 +19,63 @@ void getInputWithDisplay(int y, int x, char *input, int maxLen);
 int signUp();
 int login();
 void pregameMenu();
+
+typedef enum {
+    NORMAL_FOOD,
+    HEALING_FOOD,
+    MAGIC_FOOD,
+    POISONOUS_FOOD
+} FoodType;
+
 typedef struct {
-    int x, y, width, height;
-    int doorsX[4];
-    int doorsY[4];
-    int doorCount;
+    FoodType type;
+    int hungerEffect;
+    int healthEffect;
+    char name[20];
+    int x;
+    int y;
+} Food;
+
+typedef struct {
+    int x;
+    int y;
+    int health;
+    int hunger;
+    int inventoryCount;
+    int inventory[5];
+} Player;
+
+typedef struct {
+    int x, y, height, width;
 } Room;
+
 typedef struct {
     char tiles[MAP_HEIGHT][MAP_WIDTH];
     bool visited[MAP_HEIGHT][MAP_WIDTH];
     Room rooms[MAX_ROOMS];
     int roomCount;
-    int playerX, playerY;
+    Player player;
 } GameState;
+
 void initializeMap(GameState *game);
-bool isOverlapping(Room *a, Room *b);
-void generateRooms(GameState *game);
-void addDoors(GameState *game);
-void generateCorridors(GameState *game);
+int itoverlaps(GameState *game, int y, int x, int height, int width);
+void generateRoom(GameState *game, int y, int x, int height, int width);
+void addDoors(GameState *game, int y, int x, int height, int width);
+void generateCorridors(GameState *game, Room *rooms);
+void removeUnconnectedDoors(GameState *game);
+void generate_random_map(GameState *game);
+void resetGameState(GameState *game);
 void checkMap(GameState *game);
+void addColumnsToRooms(GameState *game);
+void initializePlayer(GameState *game);
+void movePlayer(GameState *game, char input);
+void addFoodToMap(GameState *game);
+void displayInventory(GameState *game);
+void eatFood(GameState *game, int index);
+void reduceHunger(GameState *game, int amount);
+void increaseHealth(GameState *game, int amount);
+void increaseHunger(GameState *game);
+void decreasePlayerHealth(GameState *game);
+void gameloop(GameState *game);
 
 #endif
